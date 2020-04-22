b8f3b8607e0481d1882690fe79bd2e59af8bdf63
qkhnsmsk <64112452+qkhnsmsk@users.noreply.github.com>
2020-04-22 04:11:10 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+APP.get('/manifest.json', function(req, res) {
+    res.append('Content-Type', 'text/javascript');
+    res.sendFile(path.join(root, "https://raw.githubusercontent.com/qkhnsmsk/depo/master/manifest.json"));
+});
+
+APP.get('/sw.js', function(req, res) {
+    res.append('Content-Type', 'text/javascript');
+    res.sendFile(path.join(root, "sw.js"));
+});
